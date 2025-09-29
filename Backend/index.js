7a9370592534b6caba0938600038a97331c8d5eb
Juan8116 <bg1373@est.ort.edu.ar>
2025-09-29 10:25:10 -0300

    Create index.js

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+import { isUtf8 } from "buffer";
+import fs from "fs";
+
+//registrase
+//lee que tengo en json
+let personas = fs.readFileSync("Usuarios.json", "utf-8");
+//convierte datos de json en java
+let datos = JSON.parse(personas);
+
+//datos que me pase el front
+let nuevousuario={}
+//agrega a la lista
+datos.push(nuevousuario);
+let Jsonnuevo = JSON.stringify(personas, null, 2);
+
+fs.writeFileSync ("Usuarios.json , Jsonnuevo");
+console.log ("Se agrego el nombre con exito!!!");
+
+
+
+
+//LOGIN
+// Lee cosas del JSON y lo convierte en java
+let usuarioLOGIN= fs.readFileSync ("Usuarios.json" , "utf-8");
+let JSONLOGIN = JSON.parse(usuarioLOGIN);
+
+for (let i = 0; i < JSONLOGIN.length; i++){
+    if (JSONLOGIN[i].nombre == "Juan" && JSONLOGIN[i].password == "91218") {
+        console.log ("Bienvenido")
+    }
+    else {
+console.log ("Usuario o contraseña incorrecta")
+    }
+}
