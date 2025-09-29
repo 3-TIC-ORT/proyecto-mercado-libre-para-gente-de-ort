8def5fb15912533f41078b52bef371fc21996735
Juan8116 <bg1373@est.ort.edu.ar>
2025-09-29 11:55:04 -0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,34 +1,23 @@
-import { isUtf8 } from "buffer";
 import fs from "fs";
 
-//registrase
-//lee que tengo en json
+// Leer archivo
 let personas = fs.readFileSync("Usuarios.json", "utf-8");
-//convierte datos de json en java
 let datos = JSON.parse(personas);
 
-//datos que me pase el front
-let nuevousuario={}
-//agrega a la lista
+// Nuevo usuario
+let nuevousuario = {
+  nombre: "Juan",
+  mail: "juan@ort.edu.ar",
+  sede: "Almagro",
+  password: "91218"
+};
+
+// Agregar al array
 datos.push(nuevousuario);
-let Jsonnuevo = JSON.stringify(personas, null, 2);
 
-fs.writeFileSync ("Usuarios.json , Jsonnuevo");
-console.log ("Se agrego el nombre con exito!!!");
+// Convertir a JSON
+let Jsonnuevo = JSON.stringify(datos, null, 2);
 
-
-
-
-//LOGIN
-// Lee cosas del JSON y lo convierte en java
-let usuarioLOGIN= fs.readFileSync ("Usuarios.json" , "utf-8");
-let JSONLOGIN = JSON.parse(usuarioLOGIN);
-
-for (let i = 0; i < JSONLOGIN.length; i++){
-    if (JSONLOGIN[i].nombre == "Juan" && JSONLOGIN[i].password == "91218") {
-        console.log ("Bienvenido")
-    }
-    else {
-console.log ("Usuario o contraseña incorrecta")
-    }
-}
+// Guardar en archivo
+fs.writeFileSync("Usuarios.json", Jsonnuevo);
+console.log("✅ Usuario agregado con éxito");
